793f02143b31f6077dc7f36d921b2e3ed4691310
lindakai2016 <398017990@qq.com>
2017-08-01 13:27:46 +0800

    ss

---
+++ upload/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * jQuery File Upload Plugin PHP Example
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * https://opensource.org/licenses/MIT
+ */
+require_once("UploadHandler.php");
+
+
+$handler = new UploadHandler();
+
+
+
+
